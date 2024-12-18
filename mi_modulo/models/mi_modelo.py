14dda239e98339668c13485c5ebc519fa3b04315
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-18 13:38:12 -0500

    Delete mi_modulo directory

--- mi_modulo/models/mi_modelo.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class MiModelo(models.Model):
-    _name = 'mi.modelo'
-    _description = 'Descripción de mi modelo'
-
-    name = fields.Char(string='Nombre', required=True)
-    descripcion = fields.Text(string='Descripción')
-    fecha = fields.Date(string='Fecha')
