a0e8a2098535d8adad8849ca3bbe578223613214
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 11:04:18 -0500

    Delete custom_module directory

--- custom_module/models/custom_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class CustomModel(models.Model):
-    _name = 'custom.model'
-    _description = 'Custom Model'
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
