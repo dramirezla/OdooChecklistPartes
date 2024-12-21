5406e66648cac435c8432d1df33634a237a8cb8c
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 11:23:30 -0500

    Update __manifest__.py

--- mi_modulo/__manifest__.py
+++ mi_modulo/__manifest__.py
@@ -1,12 +1,15 @@
 {
     'name': 'Mi Módulo',
     'version': '1.0',
-    'author': 'Tu Nombre',
+    'summary': 'Descripción breve del módulo',
+    'description': 'Descripción detallada del módulo',
+    'author': 'David Alejandro Ramírez',
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/mi_modelo_views.xml',
     ],
     'installable': True,
-    'application': False,
+    'application': True,
+    'license': 'LGPL-3',
 }
