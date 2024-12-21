ebde7a15d5be647607e91cbb1714267cee4f9532
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 08:41:42 -0500

    Delete simple_module directory

--- simple_module/models/simple_model.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import models, fields
-
-class SimpleModel(models.Model):
-    _name = "simple.model"
-    _description = "Simple Model"
-
-    name = fields.Char(string="Name", required=True)
