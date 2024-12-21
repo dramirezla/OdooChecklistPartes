a3e137d3825fdca07cd097dca49716c2838ec4b5
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 08:11:58 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 {
     'name': 'Mi Módulo',
-    'version': '1.0',
+    'version': '2.0',
     'summary': 'Descripción breve del módulo',
     'description': 'Descripción detallada del módulo',
     'author': 'David Alejandro Ramírez',
-    'depends': ['base'],
+    'depends': ['base','mail','account'],
     'data': [
         'security/ir.model.access.csv',
         'views/mi_modelo_views.xml',
