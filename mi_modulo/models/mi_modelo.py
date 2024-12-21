c7aec0baae65581ee4fb2e93d8ac987af72cc763
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 13:38:37 -0500

    Update mi_modelo.py

--- mi_modulo/models/mi_modelo.py
+++ mi_modulo/models/mi_modelo.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 
 class MiModelo(models.Model):
-    _name = 'mi.modelo'
+    _name = 'mimodelo2'
     _description = 'Descripción de mi modelo'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
