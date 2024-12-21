7943842389ea05718d843186132861b20841f516
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 14:06:38 -0500

    Update __manifest__.py

--- mi_modulo/__manifest__.py
+++ mi_modulo/__manifest__.py
@@ -4,6 +4,7 @@
     'summary': 'Descripción breve del módulo',
     'description': 'Modulo para la descripcion y seleccion de partes de un documento pdf',
     'author': 'David Alejandro Ramírez',
+    'category': 'Custom',
     'depends': ['base', 'mail', 'account'],
     'data': [
         'security/ir.model.access.csv',
