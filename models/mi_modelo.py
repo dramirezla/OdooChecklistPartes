d2aa7bb665c9033a124f924999dda3fbce30e004
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-21 08:01:12 -0500

    Update mi_modelo.py

--- models/mi_modelo.py
+++ models/mi_modelo.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class MiModelo(models.Model):
     _name = 'mi_modelo'
-    _description = 'Descripción de mi modelo'
+    _description = 'Modelo para la procesar las partes del pdf'
 
     name = fields.Char(string='Nombre', required=True)
     descripcion = fields.Text(string='Descripción')
