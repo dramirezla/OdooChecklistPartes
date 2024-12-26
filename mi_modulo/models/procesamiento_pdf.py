915d4d7971d5049808c5cd19e5f42ed580af3f28
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-26 12:32:22 -0500

    Update procesamiento_pdf.py

--- mi_modulo/models/procesamiento_pdf.py
+++ mi_modulo/models/procesamiento_pdf.py
@@ -71,7 +71,7 @@ class ProcesamientoPDFParte(models.Model):
     _name = 'procesamiento.pdf.parte'
     _description = 'Partes encontradas en el PDF'
 
-    pdf_id = fields.Many2one('procesamiento.pdf', string='PDF Asociado', ondelete='cascade')
+    pdf_id = fields.Many2one('procesamiento.pdf', string='PDF Asociado')
     letra = fields.Char(string='Letra Encontrada')
     layout = fields.Integer(string='Número de Página')
     seleccionada = fields.Boolean(string='Seleccionada')
