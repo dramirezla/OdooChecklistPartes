38b3f528497d52ec35185801db8e81c57f746077
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2025-01-03 18:09:19 -0500

    Update procesamiento_pdf.py

--- mi_modulo/models/procesamiento_pdf.py
+++ mi_modulo/models/procesamiento_pdf.py
@@ -7,15 +7,6 @@ import re
 from collections import Counter
 
 
-class ProcesamientoPDFParte(models.Model):
-    _name = 'procesamiento.pdf.parte'
-    _description = 'Partes encontradas en el PDF'
-
-    pdf_id = fields.Many2one('procesamiento.pdf', string='PDF Asociado')
-    letra = fields.Char(string='Letra Encontrada')
-    layout = fields.Integer(string='Número de Página')
-    seleccionada = fields.Boolean(string='Seleccionada')
-
 
 class ProcesamientoPDF(models.Model):
     _name = 'procesamiento.pdf'
@@ -86,4 +77,12 @@ class ProcesamientoPDF(models.Model):
 
         self.procesado = True
 
+class ProcesamientoPDFParte(models.Model):
+    _name = 'procesamiento.pdf.parte'
+    _description = 'Partes encontradas en el PDF'
+
+    pdf_id = fields.Many2one('procesamiento.pdf', string='PDF Asociado')
+    letra = fields.Char(string='Letra Encontrada')
+    layout = fields.Integer(string='Número de Página')
+    seleccionada = fields.Boolean(string='Seleccionada')
 
