11afa4e50efea03ab8c995d058a3495a58a6b917
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2025-01-10 12:20:23 -0500

    Update procesamiento_pdf.py

--- mi_modulo/models/procesamiento_pdf.py
+++ mi_modulo/models/procesamiento_pdf.py
@@ -59,7 +59,6 @@ class ProcesamientoPDF(models.Model):
 
             # Validar si se encontró una dimensión
             if page_num == 1:
-                raise UserError(texto)
                 match_dim = re.search(r'\[A1\]\s*(\d{1,4})[xX](\d{1,4})', texto)
                 altura_layout = int(match_dim.group(1))  # Primera captura (altura)
                 base_layout = int(match_dim.group(2))   # Segunda captura (base)
